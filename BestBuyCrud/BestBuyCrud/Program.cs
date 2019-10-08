79b66b1589459fc3fd92f350ec8d26e0a6174b03
John Clark <bennyclarkup@gmail.com>
2019-10-08 08:44:53 -0500

    Phase 1

---
+++ BestBuyCrud/BestBuyCrud/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+namespace BestBuyCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var departments = (GetAllDepartments());
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department);
+            }
+        }
+        static List<string> GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("connectionString.txt");
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT name FROM Departments;";
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read() == true)
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+                return allDepartments;
+            }
+        }
+    }
+}
+
+
+
+
+
+
